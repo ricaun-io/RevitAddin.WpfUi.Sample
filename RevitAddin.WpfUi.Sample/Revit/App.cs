996b10d2b99dcb497fd72b05f1532eb0719dda4d
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-07-23 16:05:24 -0300

    Add ResourcesDictionaries

--- RevitAddin.WpfUi.Sample/Revit/App.cs
+++ RevitAddin.WpfUi.Sample/Revit/App.cs
@@ -2,6 +2,8 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using ricaun.Revit.UI;
 using System;
+using System.Collections.Generic;
+using System.Windows;
 
 namespace RevitAddin.WpfUi.Sample.Revit
 {
@@ -19,12 +21,31 @@ namespace RevitAddin.WpfUi.Sample.Revit
             ribbonPanel.CreatePushButton<Commands.CommandSimple>("Simple")
                 .SetLargeImage("Resources/Revit.ico");
 
+            var themesDictionary = new Wpf.Ui.Markup.ThemesDictionary();
+            var controlsDictionary = new Wpf.Ui.Markup.ControlsDictionary();
+
+            ResourcesDictionaries.Add(themesDictionary);
+            ResourcesDictionaries.Add(controlsDictionary);
+
+            foreach (var item in ResourcesDictionaries)
+            {
+                //Application.Current?.Resources.MergedDictionaries.Add(item);
+            }
+
             return Result.Succeeded;
         }
+
+        private List<ResourceDictionary> ResourcesDictionaries = new List<ResourceDictionary>();
 
         public Result OnShutdown(UIControlledApplication application)
         {
             ribbonPanel?.Remove();
+
+            foreach (var item in ResourcesDictionaries)
+            {
+                Application.Current?.Resources.MergedDictionaries.Remove(item);
+            }
+
             return Result.Succeeded;
         }
     }
